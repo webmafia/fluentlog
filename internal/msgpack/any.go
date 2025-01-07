9b5eafe70efccd94b138751eea20349af0fbd0c8
Ivar Johansson <ivar@webbmaffian.se>
2025-01-07 09:18:21 +0100

    msgpack: AppendAny

---
+++ internal/msgpack/any.go
@@ -0,0 +1,66 @@
+package msgpack
+
+import (
+	"fmt"
+
+	"github.com/webmafia/fluentlog/internal"
+)
+
+func AppendAny(dst []byte, v any) []byte {
+	switch val := v.(type) {
+
+	case []byte:
+		dst = AppendBinary(dst, val)
+
+	case bool:
+		dst = AppendBool(dst, val)
+
+	case float32:
+		dst = AppendFloat(dst, float64(val))
+
+	case float64:
+		dst = AppendFloat(dst, val)
+
+	case int:
+		dst = AppendInt(dst, int64(val))
+
+	case int8:
+		dst = AppendInt(dst, int64(val))
+
+	case int16:
+		dst = AppendInt(dst, int64(val))
+
+	case int32:
+		dst = AppendInt(dst, int64(val))
+
+	case int64:
+		dst = AppendInt(dst, val)
+
+	case uint:
+		dst = AppendUint(dst, uint64(val))
+
+	case uint8:
+		dst = AppendUint(dst, uint64(val))
+
+	case uint16:
+		dst = AppendUint(dst, uint64(val))
+
+	case uint32:
+		dst = AppendUint(dst, uint64(val))
+
+	case uint64:
+		dst = AppendUint(dst, val)
+
+	case string:
+		dst = AppendString(dst, val)
+
+	case internal.TextAppender:
+		dst = AppendTextAppender(dst, val)
+
+	case fmt.Stringer:
+		dst = AppendString(dst, val.String())
+
+	}
+
+	return dst
+}
