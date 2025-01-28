9f0ee897ab1c255fdeb26efb0945f9d453470bbf
Ivar Johansson <ivar@webbmaffian.se>
2025-01-28 11:28:51 +0100

    WIP: BinReader

--- internal/msgpack/binary_reader.go
+++ internal/msgpack/binary_reader.go
@@ -10,49 +10,36 @@ type binReader struct {
 
 // Read implements io.Reader.
 func (b binReader) Read(p []byte) (n int, err error) {
+	if b.iter.remain < len(p) {
+		p = p[:b.iter.remain]
+	}
+
 	// If there's nothing left to read, return EOF
 	if b.iter.remain == 0 {
 		return 0, io.EOF
 	}
 
 	// Read from both the buffer and the io.Reader
-	for n < len(p) && b.iter.remain > 0 {
+	for n < len(p) {
+
 		// Determine how many bytes can be read
-		toCopy := len(p) - n
-		available := len(b.iter.buf) - b.iter.n
+		bufLen := len(b.iter.buf)
+		unreadLen := bufLen - b.iter.n
 
-		// First, read from the buffer
-		if available > 0 {
-			if toCopy > available {
-				toCopy = available
-			}
-			if toCopy > b.iter.remain {
-				toCopy = b.iter.remain
-			}
+		// First, read from the iterator's buffer
+		if unreadLen > 0 {
+			copied := copy(p[n:], b.iter.buf[b.iter.n:])
+			// b.iter.buf = b.iter.buf[:bufLen-copied]
+			b.iter.n += copied
+			n += copied
+		} else {
 
-			copy(p[n:], b.iter.buf[b.iter.n:b.iter.n+toCopy])
-			b.iter.n += toCopy
-			b.iter.remain -= toCopy
-			n += toCopy
-		}
-
-		// Then, read from the io.Reader if needed
-		if n < len(p) && b.iter.remain > 0 {
-			toRead := len(p) - n
-			if toRead > b.iter.remain {
-				toRead = b.iter.remain
-			}
-
-			readBytes, readErr := b.iter.r.Read(p[n : n+toRead])
+			// Then, read the rest directly from the io.Reader
+			readBytes, readErr := b.iter.r.Read(p[n:])
 			n += readBytes
-			b.iter.remain -= readBytes
 
 			if readErr != nil {
-				if readErr == io.EOF && b.iter.remain == 0 {
-					err = io.EOF
-				} else {
-					err = readErr
-				}
+				err = readErr
 				break
 			}
 		}
@@ -64,5 +51,7 @@ func (b binReader) Read(p []byte) (n int, err error) {
 	}
 
 	b.iter.tot += n
-	return n, err
+	b.iter.remain -= n
+
+	return
 }
