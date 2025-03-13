b2894f50f8dbc53e106ed75fda2c4571af6dd430
Ivar Johansson <ivar@webbmaffian.se>
2025-03-13 14:14:24 +0100

    Added JSON support on identifier

--- pkg/identifier/id_coder.go
+++ pkg/identifier/id_coder.go
@@ -3,6 +3,7 @@ package identifier
 import (
 	"encoding/binary"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 
 	"github.com/webmafia/fast"
@@ -17,6 +18,8 @@ const (
 var (
 	_ internal.TextAppender   = ID(0)
 	_ internal.BinaryAppender = ID(0)
+	_ json.Marshaler          = ID(0)
+	_ json.Unmarshaler        = (*ID)(nil)
 )
 
 func IDFromString(str string) (id ID, err error) {
@@ -57,4 +60,24 @@ func (id ID) AppendText(b []byte) ([]byte, error) {
 // AppendBinary implements internal.BinaryAppender.
 func (id ID) AppendBinary(b []byte) ([]byte, error) {
 	return binary.BigEndian.AppendUint64(b, uint64(id)), nil
+}
+
+// MarshalJSON implements json.Marshaler.
+func (id ID) MarshalJSON() (b []byte, err error) {
+	b = make([]byte, 0, 18)
+	b = append(b, '"')
+	b, err = id.AppendText(b)
+	b = append(b, '"')
+
+	return
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (id *ID) UnmarshalJSON(b []byte) (err error) {
+	if len(b) != 18 || b[0] != '"' || b[17] != '"' {
+		return errors.New("invalid ID")
+	}
+
+	*id, err = IDFromString(fast.BytesToString(b[1:17]))
+	return
 }
