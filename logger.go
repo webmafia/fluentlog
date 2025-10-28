49a9c8ef03a01f4e7fe6fd8ce76caece209064c2
Ivar Johansson <ivar@webbmaffian.se>
2025-10-28 11:52:59 +0100

    Added method for all severity levels

--- logger.go
+++ logger.go
@@ -17,72 +17,84 @@ func NewLogger(inst *Instance) *Logger {
 	return inst.Logger()
 }
 
-// Logs a new entry of "Debug" severity, with optional meta data. Example usage:
-//
-//	log.Debug("hello world",
-//	    "myKey", "myValue",
-//	    "otherKey", 123,
-//	)
+// Debug or trace information.
 func (l *Logger) Debug(msg string, args ...any) hexid.ID {
 	return l.inst.log(DEBUG, msg, args, false, 4, l.fieldData, l.fieldCount)
 }
 
-// Logs a new entry of "Info" severity, with optional meta data. Example usage:
-//
-//	log.Info("hello world",
-//	    "myKey", "myValue",
-//	    "otherKey", 123,
-//	)
+// Routine information, such as ongoing status or performance.
 func (l *Logger) Info(msg string, args ...any) hexid.ID {
 	return l.inst.log(INFO, msg, args, false, 4, l.fieldData, l.fieldCount)
 }
 
-// Logs a new entry of "Warning" severity, with optional meta data. Example usage:
-//
-//	log.Warn("hello world",
-//	    "myKey", "myValue",
-//	    "otherKey", 123,
-//	)
+// Normal but significant events, such as start up, shut down, or a configuration change.
+func (l *Logger) Notice(msg string, args ...any) hexid.ID {
+	return l.inst.log(NOTICE, msg, args, false, 4, l.fieldData, l.fieldCount)
+}
+
+// Warning events might cause problems.
 func (l *Logger) Warn(msg string, args ...any) hexid.ID {
 	return l.inst.log(WARN, msg, args, false, 4, l.fieldData, l.fieldCount)
 }
 
-// Logs a new entry of "Error" severity, with optional meta data. Example usage:
-//
-//	log.Error("hello world",
-//	    "myKey", "myValue",
-//	    "otherKey", 123,
-//	)
+// Error events are likely to cause problems.
 func (l *Logger) Error(msg string, args ...any) hexid.ID {
 	return l.inst.log(ERR, msg, args, false, 4, l.fieldData, l.fieldCount)
 }
 
-// Logs a new entry of "Debug" severity, with a formatted (printf) message. Example usage:
-//
-//	log.Debugf("the number is %d", 123)
+// Critical events cause more severe problems or outages.
+func (l *Logger) Crit(msg string, args ...any) hexid.ID {
+	return l.inst.log(CRIT, msg, args, false, 4, l.fieldData, l.fieldCount)
+}
+
+// A person must take an action immediately.
+func (l *Logger) Alert(msg string, args ...any) hexid.ID {
+	return l.inst.log(ALERT, msg, args, false, 4, l.fieldData, l.fieldCount)
+}
+
+// One or more systems are unusable.
+func (l *Logger) Emerg(msg string, args ...any) hexid.ID {
+	return l.inst.log(EMERG, msg, args, false, 4, l.fieldData, l.fieldCount)
+}
+
+// Debug or trace information. Formatted with printf syntax.
 func (l *Logger) Debugf(format string, args ...any) hexid.ID {
 	return l.inst.log(DEBUG, format, args, true, 4, l.fieldData, l.fieldCount)
 }
 
-// Logs a new entry of "Info" severity, with a formatted (printf) message. Example usage:
-//
-//	log.Infof("the number is %d", 123)
+// Routine information, such as ongoing status or performance. Formatted with printf syntax.
 func (l *Logger) Infof(format string, args ...any) hexid.ID {
 	return l.inst.log(INFO, format, args, true, 4, l.fieldData, l.fieldCount)
 }
 
-// Logs a new entry of "Warning" severity, with a formatted (printf) message. Example usage:
-//
-//	log.Warnf("the number is %d", 123)
+// Normal but significant events, such as start up, shut down, or a configuration change. Formatted with printf syntax.
+func (l *Logger) Noticef(msg string, args ...any) hexid.ID {
+	return l.inst.log(NOTICE, msg, args, true, 4, l.fieldData, l.fieldCount)
+}
+
+// Warning events might cause problems. Formatted with printf syntax.
 func (l *Logger) Warnf(format string, args ...any) hexid.ID {
 	return l.inst.log(WARN, format, args, true, 4, l.fieldData, l.fieldCount)
 }
 
-// Logs a new entry of "Error" severity, with a formatted (printf) message. Example usage:
-//
-//	log.Errorf("the number is %d", 123)
+// Error events are likely to cause problems. Formatted with printf syntax.
 func (l *Logger) Errorf(format string, args ...any) hexid.ID {
 	return l.inst.log(ERR, format, args, true, 4, l.fieldData, l.fieldCount)
+}
+
+// Critical events cause more severe problems or outages. Formatted with printf syntax.
+func (l *Logger) Critf(msg string, args ...any) hexid.ID {
+	return l.inst.log(CRIT, msg, args, true, 4, l.fieldData, l.fieldCount)
+}
+
+// A person must take an action immediately. Formatted with printf syntax.
+func (l *Logger) Alertf(msg string, args ...any) hexid.ID {
+	return l.inst.log(ALERT, msg, args, true, 4, l.fieldData, l.fieldCount)
+}
+
+// One or more systems are unusable. Formatted with printf syntax.
+func (l *Logger) Emergf(msg string, args ...any) hexid.ID {
+	return l.inst.log(EMERG, msg, args, true, 4, l.fieldData, l.fieldCount)
 }
 
 // Logs metric values. Example usage:
